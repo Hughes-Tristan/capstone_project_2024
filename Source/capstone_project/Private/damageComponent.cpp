7b11cb9b6bbb6bee0381789b6add89f9f9185063
Hughes-Tristan <Hughes-Tristan@users.noreply.github.com>
2024-12-07 01:15:12 -0800

    Enemy Damage Implemented
    - enemy now attacks the character using the modular damage system

--- Source/capstone_project/Private/damageComponent.cpp
+++ Source/capstone_project/Private/damageComponent.cpp
@@ -21,6 +21,7 @@ UdamageComponent::UdamageComponent() {
 	PrimaryComponentTick.bCanEverTick = true;
 	// Default Health Value
 	health = 100.0;
+	isDead = false;
 }
 
 
@@ -58,6 +59,10 @@ void UdamageComponent::applyDamage(const UdamageInfo* damageInfo) {
 			TEXT("No Damage Taken");
 			break;
 
+		case EDamageType::EnemyAttack:
+			damageApplied = 5;
+			break;
+
 		case EDamageType::LightAttack:
 			damageApplied = 24;
 			break;
@@ -78,6 +83,7 @@ void UdamageComponent::applyDamage(const UdamageInfo* damageInfo) {
 		if (health <= 0) {
 			health = 0;
 			death();
+			isDead = true;
 		}
 		else {
 			switch (damageInfo->damageResponse) {
@@ -113,7 +119,7 @@ float UdamageComponent::getMaxHealth() const {
 // this function is responsible for handling death mechanics
 // it is used to properly set the actor to ragdoll and for the capsule collision to be removed after death
 void UdamageComponent::death() {
-	isDead = true;
+	
 	ACharacter* characterCast = Cast<ACharacter>(GetOwner());
 	characterCast->GetMesh()->SetSimulatePhysics(true);
 	characterCast->GetMesh()->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
@@ -123,8 +129,8 @@ void UdamageComponent::death() {
 	}
 	characterCast->GetCharacterMovement()->DisableMovement();
 
-	Aenemycharacter1* enemyCharacter = Cast<Aenemycharacter1>(characterCast);
-	enemyCharacter->destroy();
+	//Aenemycharacter1* enemyCharacter = Cast<Aenemycharacter1>(characterCast);
+	//enemyCharacter->destroy();
 	//characterCast->GetMesh()->WakeAllRigidBodies();
 }
 
