152c25d29b0fadb2123ad2a71ae90ab45daab0d6
Hughes-Tristan <Hughes-Tristan@users.noreply.github.com>
2025-04-29 23:10:11 -0800

    Bug fix for building the project

--- Source/capstone_project/Private/developmentGamemodeTH.cpp
+++ Source/capstone_project/Private/developmentGamemodeTH.cpp
@@ -5,10 +5,5 @@
 
 AdevelopmentGamemode::AdevelopmentGamemode()
 {
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<UClass> PlayerPawnBPClass(TEXT("/Game/Content/Characters/Development/BP_developmentCharacter_TJ.BP_developmentCharacter_TJ_C"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+
 }
